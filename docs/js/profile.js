b8c371c8645339384b14ea592f6bcf3a2a4e7b74
Jens Andreasson <jens.andreasson@ga.ntig.se>
2021-09-22 08:57:50 +0200

    🎨 style

--- docs/js/profile.js
+++ docs/js/profile.js
@@ -2,79 +2,65 @@ window.addEventListener('load', async () => {
     if (!githubUser) return;
 
     axios
-    .get(`https://api.github.com/users/${githubUser}`)
-    .then(function (response) {
-        // handle success
-        console.log(response);
+        .get(`https://api.github.com/users/${githubUser}`)
+        .then(function (response) {
+            const template = document.querySelector('template');
+            const clone = template.content.cloneNode(true);
 
-        const template = document.querySelector('template');
-        const clone = template.content.cloneNode(true);
-
-        if (response.data.avatar_url) {
-            clone.querySelector('.profile__portrait').src = response.data.avatar_url;
-        } else {
-            clone.querySelector('.profile__portrait').src = `https://robohash.org/${githubUser}`;
-        }
-
-        clone.querySelector('.profile__heading').textContent = response.data.name;
-        const login = document.createElement('span');
-        login.textContent = response.data.login;
-        login.classList.add('profile__lead');
-        clone.querySelector('.profile__body').insertBefore(login, clone.querySelector('.profile__text'));
-        
-        clone.querySelector('.profile__body').querySelector('.profile__text').textContent = response.data.bio;
-
-        const listData = {            
-            organization: response.data.company,
-            location: response.data.location,
-            email: response.data.email,
-            link: response.data.blog
-        };
-
-        for (const [key, value] of Object.entries(listData)) {
-            if (!value) continue;
-            const li = document.createElement('li');
-            const icon = document.createElement('img');
-            icon.src = `images/${key}.svg`;
-            icon.alt = `Icon for ${key}`;
-            li.classList.add('profile__list-item');
-            if (key === 'link') {
-                const a = document.createElement('a');
-                a.href = `https://${value}`;
-                a.textContent = value;
-                li.appendChild(a);
+            if (response.data.avatar_url) {
+                clone.querySelector('.profile__portrait').src =
+                    response.data.avatar_url;
             } else {
-                li.textContent = value;
+                clone.querySelector(
+                    '.profile__portrait'
+                ).src = `https://robohash.org/${githubUser}`;
             }
-            li.prepend(icon);
-            clone.querySelector('.profile__list').appendChild(li);
-        }
 
-        // listData.forEach(item => {
-        //     if (!item) return;
-        //     const li = document.createElement('li');
-        //     li.textContent = item;
-        //     clone.querySelector('.profile__list').appendChild(li);
-        // });
+            clone.querySelector('.profile__heading').textContent =
+                response.data.name;
+            const login = document.createElement('span');
+            login.textContent = response.data.login;
+            login.classList.add('profile__lead');
+            clone
+                .querySelector('.profile__body')
+                .insertBefore(login, clone.querySelector('.profile__text'));
 
-        const body = document.querySelector('body');
-        body.appendChild(clone);
-        
-        // response.data.blog
-        // response.data.email
-        // response.data.bio
+            clone
+                .querySelector('.profile__body')
+                .querySelector('.profile__text').textContent =
+                response.data.bio;
 
-        // response.data.public_repos
+            const listData = {
+                organization: response.data.company,
+                location: response.data.location,
+                email: response.data.email,
+                link: response.data.blog
+            };
 
-        // reponse.data.url
+            for (const [key, value] of Object.entries(listData)) {
+                if (!value) continue;
+                const li = document.createElement('li');
+                const icon = document.createElement('img');
+                icon.src = `images/${key}.svg`;
+                icon.alt = `Icon for ${key}`;
+                li.classList.add('profile__list-item');
+                if (key === 'link') {
+                    const a = document.createElement('a');
+                    a.href = `https://${value}`;
+                    a.textContent = value;
+                    li.appendChild(a);
+                } else {
+                    li.textContent = value;
+                }
+                li.prepend(icon);
+                clone.querySelector('.profile__list').appendChild(li);
+            }
 
-    })
-    .catch(function (error) {
-    // handle error
-    console.log(error);
-    })
-    .then(function () {
-    // always executed
-    });
-
+            const body = document.querySelector('body');
+            body.appendChild(clone);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
 });
